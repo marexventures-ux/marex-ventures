b5fe84264d0c6d73b416adb8838f7a1c8b597de8
marexventures-ux <marexventures@gmail.com>
2026-06-01 09:56:39 +0100

    Add files via upload

---
+++ eleventy.config.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+  // Passthrough copy for static assets
+  eleventyConfig.addPassthroughCopy("src/css");
+  eleventyConfig.addPassthroughCopy("src/js");
+  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("src/admin");
+  eleventyConfig.addPassthroughCopy("src/_redirects");
+  
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "includes"
+    }
+  };
+};
