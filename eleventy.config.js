4920e1861a5250ab203337040aa3e8553a1ceef5
marexventures-ux <marexventures@gmail.com>
2026-09-14 23:23:40 +0100

    Add files via upload

--- eleventy.config.js
+++ eleventy.config.js
@@ -7,6 +7,20 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/_redirects");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   
+  // Custom collection for blog posts: excludes drafts & future scheduled posts
+  eleventyConfig.addCollection("blog", function(collectionApi) {
+    const now = new Date();
+    return collectionApi.getFilteredByTag("blog")
+      .filter(item => {
+        // Exclude drafts
+        if (item.data.draft === true) return false;
+        // Exclude future scheduled posts
+        if (item.date && item.date > now) return false;
+        return true;
+      })
+      .sort((a, b) => b.date - a.date);
+  });
+
   return {
     dir: {
       input: "src",
